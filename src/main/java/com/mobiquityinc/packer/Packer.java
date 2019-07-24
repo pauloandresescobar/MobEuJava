e6df2d50ae5cae5698df22add69e9633a605111d
paulo.escobar <ingenieropauloescobar@gmail.com>
2019-07-24 16:22:11 -0500

    added some documentation

--- src/main/java/com/mobiquityinc/packer/Packer.java
+++ src/main/java/com/mobiquityinc/packer/Packer.java
@@ -12,7 +12,7 @@ public class Packer {
    *
    * @param filePath is the file to read and process
    * @return a string with the selected packages to send separated by a coma
-   * @throws APIException if:
+   * @throws APIException if this conditions are not true:
    * 1. Max weight that a package can take is ≤ 100
    * 2. There might be up to 15 items you need to choose from
    * 3. Max weight and cost of an item is ≤ 100
